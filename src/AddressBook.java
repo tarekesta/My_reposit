d7566d4ebd707c1e5508cef8187cf440742c191b
Tarek Esta <tarekesta@cmail.carleton.ca>
2023-10-01 18:43:24 -0400

    nFirst time

---
+++ src/AddressBook.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    public ArrayList<BuddyInfo> collection;
+
+    public AddressBook() {
+        collection = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo b_info) {
+        collection.add(b_info);
+    }
+
+    public boolean removeBuddy(BuddyInfo b_info) {
+        return collection.remove(b_info);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+
+}
