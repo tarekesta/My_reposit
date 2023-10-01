7981941450e9e1c8d028830c020fcea8fdcc9e85
Tarek Esta <tarekesta@cmail.carleton.ca>
2023-10-01 18:46:51 -0400

    nSecnd

---
+++ src/BuddyInfo.java
@@ -0,0 +1,32 @@
+public class BuddyInfo {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BuddyInfo(String name) {
+        this.name = name;
+    }
+
+
+    public BuddyInfo() {
+
+    }
+
+
+    private String name;
+
+    public static void main(String[] args) {
+        BuddyInfo bd = new BuddyInfo();
+        bd.setName("Tarek");
+        System.out.println("Hello " + bd.getName());
+        AddressBook adb = new AddressBook();
+        adb.addBuddy(bd);
+        System.out.println(adb.removeBuddy(bd));
+    }
+
+
+}
